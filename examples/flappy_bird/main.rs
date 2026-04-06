7dabeb7e399223ef32d7628d3f81ad64d7f58255
Srayan Jana <valorzard.kiko@outlook.com>
2026-04-06 16:48:59 -0700

    add a game over

--- examples/flappy_bird/main.rs
+++ examples/flappy_bird/main.rs
@@ -64,7 +64,7 @@ fn create_bird(engine: &mut Engine) -> Mesh {
         Mesh::new(geo_handle, mat_handle)
 }
 
-fn create_pipe(engine: &mut Engine) -> Mesh {
+fn create_pipe(engine: &mut Engine, turn_up: bool) -> Mesh {
     // 1. Create quad geometry
         let mut geometry = Geometry::new();
         geometry.set_attribute(
@@ -96,9 +96,11 @@ fn create_pipe(engine: &mut Engine) -> Mesh {
         geometry.set_indices(&[0, 2, 1, 1, 2, 3]);
 
         // 2. Create unlit material with a solid color texture
-        let image = image::load_from_memory(PIPE_IMAGE_DATA).expect("failed to decode PNG");
-         // flip image since its upside down by default
-        let image = image.flipv();
+        let mut image = image::load_from_memory(PIPE_IMAGE_DATA).expect("failed to decode PNG");
+        if turn_up {
+            // flip image since its upside down by default
+            image = image.flipv();
+        }
         let decoded = image.to_rgba8();
         let (w, h) = (decoded.width(), decoded.height());
         let image_handle = engine
@@ -122,6 +124,7 @@ fn create_pipe(engine: &mut Engine) -> Mesh {
 }
 
 const PIPE_SPAWN_INTERVAL: f32 = 2.0; // seconds
+const BIRD_SPAWN_POINT : Vec3 = Vec3::new(-1.0, 0.0, 0.0);
 struct FlappyBird {
     bird_node: NodeHandle,
     bird_velocity: Vec2,
@@ -132,16 +135,20 @@ impl AppHandler for FlappyBird {
     fn init(engine: &mut Engine, _window: &dyn Window) -> Self {
 
         let bird_mesh = create_bird(engine);
-        let pipe_mesh = create_pipe(engine);
+        let pipe_mesh = create_pipe(engine, true);
         engine.scene_manager.create_active();
         let scene = engine.scene_manager.active_scene_mut().unwrap();        
 
         let bird_node = scene.add_mesh(bird_mesh);
         if let Some(node) = scene.get_node_mut(bird_node) {
-            node.transform.position = Vec3::new(1.0, 1.0, 0.0);
+            node.transform.position = BIRD_SPAWN_POINT;
             node.transform.scale = Vec3::new(0.1, 0.1, 0.1);
         }
         let pipe_node = scene.add_mesh(pipe_mesh);
+        if let Some(node) = scene.get_node_mut(pipe_node) {
+            node.transform.position = Vec3::new(2.0, -0.5, 0.0);
+            node.transform.scale = Vec3::new(0.5, 0.5, 1.0);
+        }
         // 5. Set up camera
         let camera = Camera::new_perspective(45.0, 1280.0 / 720.0, 0.1);
         let cam_node_id = scene.add_camera(camera);
@@ -164,17 +171,22 @@ impl AppHandler for FlappyBird {
 
     fn update(&mut self, engine: &mut Engine, window: &dyn Window, frame: &FrameState) {
         // Apply gravity
-        self.bird_velocity.y -= 9.8 * frame.dt;
+        self.bird_velocity.y -= 5.0 * frame.dt;
 
         // Detect spacebar press (flap)
         if engine.input.get_key_down(Key::Space) {
             // apply upward velocity to bird
-            self.bird_velocity.y = 10.0;
+            self.bird_velocity.y = 4.0;
         }
 
         // Update bird position
         if let Some(node) = engine.scene_manager.active_scene_mut().unwrap().get_node_mut(self.bird_node) {
             node.transform.position.y += self.bird_velocity.y * frame.dt;
+            if node.transform.position.y < -1.9 || node.transform.position.y > 1.9 {
+                println!("Game Over!");
+                node.transform.position = BIRD_SPAWN_POINT;
+                self.bird_velocity = Vec2::ZERO;
+            }
         }
     }
 }
